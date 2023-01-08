4314bc03844e393bd066aa6ef061d4e4b28a1f96
SeannnWang <w0978317621@gmail.com>
2023-01-08 12:31:09 +0800

    create retry mechanism for client side

--- client.cpp
+++ client.cpp
@@ -4,14 +4,20 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/errno.h>
+#include<sys/time.h>
+#include<unistd.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<fcntl.h>
 #include<netdb.h>
 #include<signal.h>
+#include<math.h>
 
 #define SRV_PORT 16384
 #define MAXLEN 1024
+#define MULTIPLIER 2
+#define RETRYBASE 0.5
+#define MAXINTERVAL 8
 
 int main(int argc, char **argv){
   int socketfd;
@@ -24,7 +30,12 @@ int main(int argc, char **argv){
   in_addr_t host_ip;
   struct sockaddr_in client_addr;
   struct sockaddr_in srv_addr;
-
+  /*variables which handle time out*/
+  fd_set timeout_read_fds;
+  int ret;
+  /*Max retry*/
+  int max_retry = 3;
+    
   /*Check input parameter*/
   if(argc<3){
     printf("invalid client side usage, check client format and retry\n");
@@ -79,22 +90,66 @@ int main(int argc, char **argv){
   if(nbyte>MAXLEN){
     printf("Message exceed max length\n");   
     exit(-1);
-  }
+  }    
 
-  /*send message to server*/
+  /*
+   * Set up Time out handler
+   */
+  int retry_num = 0; 
+  struct timeval timeout;
+  timeout.tv_sec = RETRYBASE*pow(MULTIPLIER,retry_num);
+  timeout.tv_usec = 0;
+  
+  //set time out check descriptor
+  FD_ZERO(&timeout_read_fds);
+  FD_SET(socketfd, &timeout_read_fds);
+
+
+  /*
+   *send message to server
+   */
   addr_length = sizeof(srv_addr);
 
-  if(sendto(socketfd, read_data, nbyte, 0, (struct sockaddr*)&srv_addr, addr_length)<0){
-    perror("Datagram sending error\n");
-    exit(-1);
-  }
+  while(true){
+    if(sendto(socketfd, read_data, nbyte, 0, (struct sockaddr*)&srv_addr, addr_length)<0){
+      perror("Datagram sending error\n");
+      exit(-1);
+    }
+ 
+    /*
+     *receive message from server
+     */  
+    ret = select(socketfd+1, &timeout_read_fds, NULL, NULL, &timeout);
 
-  if(recvfrom(socketfd, srv_response, MAXLEN, 0, (struct sockaddr*)&srv_addr,&addr_length)<0){
-    perror("Datagram reading error\n");
-    exit(-1);
-  }
-  else{
-    printf("server response: %s", srv_response);
-    exit(0);
-  }
+    if(ret<0){
+      perror("select failed");
+      exit(-1);
+    }
+    else if(ret==0){
+      //timeout occurred, implement exponential backoff
+      double interval = RETRYBASE*pow(MULTIPLIER,retry_num);      
+      printf("Timeout in %.3f sec, ", interval); 
+      
+      if(++retry_num > max_retry){
+        printf("exceed max retry:%d, exit program with code '1'", max_retry);
+        exit(1);
+      }
+      if(interval > MAXINTERVAL){
+        printf("\nExceed MAXINTERVAL:%d, exit program with code(1)\n", MAXINTERVAL);
+        exit(1);
+      }
+      printf("trigger retry mechanism\n\nRetry: %d times:\n", retry_num);         
+      timeout.tv_sec = RETRYBASE*pow(MULTIPLIER,retry_num);
+      continue;
+    }   
+    else{
+      //data is available on socket
+      if(recvfrom(socketfd, srv_response, MAXLEN, 0, (struct sockaddr*)&srv_addr,&addr_length)<0){
+        perror("Datagram reading error\n");
+        exit(-1);
+      }      
+        printf("server response: %s", srv_response);
+        exit(0);
+    }  
+  }         
 }
